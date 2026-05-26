d2d2d2dc4dec8a96af7f6cc0ec3dd6c19bd32cb0
chloemjoujip <chloemjoujip@gmail.com>
2026-05-26 17:36:18 +0100

    Update Personne.java

--- Personne.java
+++ Personne.java
@@ -1,1 +1,31 @@
 classe personne
+public abstract class Personne {
+
+    private String id;
+    private String nom;
+    private String prenom;
+    private String contact;
+
+    public Personne(String id, String nom,
+                    String prenom, String contact) {
+        this.id      = id;
+        this.nom     = nom;
+        this.prenom  = prenom;
+        this.contact = contact;
+    }
+
+    public String getId()      { return id; }
+    public String getNom()     { return nom; }
+    public String getPrenom()  { return prenom; }
+    public String getContact() { return contact; }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + prenom + " " + nom;
+    }
+}
+
+
+
+
+
